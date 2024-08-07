5b999fb28fd79611ee0e851006882303e910d253
KumarKeshavCode <kumarkeshav03030@gmail.com>
2024-08-07 11:42:15 +0530

    ADD: Method Decorator Example

--- src/decorator.ts
+++ src/decorator.ts
@@ -32,6 +32,8 @@ class car {
 const car1 = new car('BMW');
 */
 
+
+/*
 function BaseEntity(ctr : Function){
     ctr.prototype.id=Math.random();
     ctr.prototype.created = new Date().toString;
@@ -47,3 +49,30 @@ class User{
 let user =new User("dany");
 console.log(user.id);
 
+*/
+
+
+function logExecutionTime(target : any ,propertyName: string , descriptor:PropertyDescriptor){
+    let originalMethod = descriptor.value;
+    descriptor.value = function(...args: any[]){
+        console.log('Method :'  +propertyName + "execution started" ,args );
+        args[1]=10;
+        const result = originalMethod.apply(this, args);
+        console.log(`Method ${propertyName} execution ended`);
+  return result;
+    }
+   // return descriptor;
+}
+
+class Calculator{
+    @logExecutionTime
+    add(a:number, b:number):number{
+        return a + b;
+    }
+}
+
+const calculator = new Calculator();
+let total = calculator.add(4,3);
+
+
+console.log(total);
